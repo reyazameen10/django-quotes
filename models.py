582dc8015c64222ef1b6c134b340bcf27a586c2d
reyazameen10 <reyaz.am10@gmail.com>
2026-02-13 12:34:11 -0500

    Created Profile model for mini_insta

--- models.py
+++ models.py
@@ -1,3 +1,16 @@
+#mini_insta/models.py
+
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    '''Encapsulates a user profile.'''
+
+
+    #define the attribute of the profile
+    username = models.TextField(blank=False)
+    display_name = models.TextField(blank=True)
+    profile_image = models.TextField(blank=True)
+    profile_image_url = models.TextField(blank=True)
+    bio_text = models.TextField(blank=True)
+    join_date = models.DateTimeField(auto_now=True)
