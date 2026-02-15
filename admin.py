bc9b6e7d9996b1a12014e4649ab07a5f5e748f62
reyazameen10 <reyaz.am10@gmail.com>
2026-02-15 01:09:27 -0500

    last changes for mini_insta

--- admin.py
+++ admin.py
@@ -1,3 +1,9 @@
+# mini_insta/admin.py
+
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('username', 'display_name', 'profile_image', 'profile_image_url', 'bio_text', 'join_date', 'image_url')
--- models.py
+++ models.py
@@ -1,16 +1,16 @@
 #mini_insta/models.py
 
-from django.db import models
-
 # Create your models here.
+from django.db import models
 class Profile(models.Model):
-    '''Encapsulates a user profile.'''
-
-
-    #define the attribute of the profile
-    username = models.TextField(blank=False)
-    display_name = models.TextField(blank=True)
-    profile_image = models.TextField(blank=True)
-    profile_image_url = models.TextField(blank=True)
-    bio_text = models.TextField(blank=True)
-    join_date = models.DateTimeField(auto_now=True)
+    """Encapsulates a user profile."""
+    
+    username = models.CharField(max_length=50, unique=True)
+    display_name = models.CharField(max_length=100)
+    profile_image_url = models.URLField(blank=True)
+    bio_text = models.TextField(blank=True, max_length=500)
+    join_date = models.DateField()
+    
+    def __str__(self):
+        """Return a string representation of this Profile."""
+        return f'{self.username}'
